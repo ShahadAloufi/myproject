8df5820c09783c53e180fdfdc29c9e8e6b2fbbd1
ShahadAloufi <shahdse001@gmail.com>
2022-12-15 09:54:27 +0300

    Initial Commit

---
+++ myproject/myprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myprojectApp.swift
+//  myproject
+//
+//  Created by shahadmufleh on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct myprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
